eb0a04a2d69b2bf26e13b75a763d9d59d7bbac1b
Souza-JV <senhorqoficial@gmail.com>
2025-09-09 13:00:26 -0300

    criado att016

--- atividades/att003-vetores-compostos/att016.js
+++ atividades/att003-vetores-compostos/att016.js
@@ -1,7 +1,15 @@
 let vaga = [5, 8, 2, 9, 3]
-
 vaga.push(1) //adiciona o proximo num
 vaga.sort() //deixa em ordem
+
 console.log(vaga)
 console.log(`O vetor tem ${vaga.length} posições`)
 console.log(`O primeiro valor do vetor é ${vaga[0]}`)
+
+let pos = vaga.indexOf(8)
+
+if ( pos == -1) {
+    console.log('O valor não foi encontrado')
+} else {
+    console.log(`O valor 8 esta na posição ${pos}`)
+}
